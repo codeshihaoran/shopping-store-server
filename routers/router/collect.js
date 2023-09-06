9032d891ef6e101a6575dab32f6dc03dd3f1c540
shihaoran <shihaoran.code@gmail.com>
2023-09-06 20:45:44 +0800

    fix: api-Path

--- routers/router/collect.js
+++ routers/router/collect.js
@@ -2,6 +2,6 @@ const auth = require('../../middlewares/auth')
 const Router = require('koa-router')
 const collectRouter = new Router()
 const collectController = require('../../controllers/collect')
-collectRouter.post('/user/collect/addCollect', auth.checkIsLogin, collectController.addCollectProducts)
-collectRouter.post('/user/collect/getCollect', auth.checkIsLogin, collectController.getCollectProducts)
+collectRouter.post('/user/collect/add', auth.checkIsLogin, collectController.addCollectProducts)
+collectRouter.post('/user/collect/get', auth.checkIsLogin, collectController.getCollectProducts)
 module.exports = collectRouter
--- routers/router/order.js
+++ routers/router/order.js
@@ -2,6 +2,6 @@ const Router = require('koa-router')
 const orderRouter = new Router()
 const auth = require('../../middlewares/auth')
 const orderController = require('../../controllers/order')
-orderRouter.post('/user/order/addOrder', auth.checkIsLogin, orderController.addProductsToOrder)
-orderRouter.post('/user/order/getOrder', auth.checkIsLogin, orderController.getOrderInfo)
+orderRouter.post('/user/order/add', auth.checkIsLogin, orderController.addProductsToOrder)
+orderRouter.post('/user/order/get', auth.checkIsLogin, orderController.getOrderInfo)
 module.exports = orderRouter
